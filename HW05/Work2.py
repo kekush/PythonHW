64a0ddd806894e2654ed77e1f5154e2867db98d4
Borovkov A <kekushf@gmail.com>
2023-02-03 19:28:25 +0300

    Task2

---
+++ HW05/Work2.py
@@ -0,0 +1,25 @@
+# Создайте программу для игры с конфетами человек против человека.
+import random
+
+conf = 20
+step = 5
+# num = conf + 1
+player = random.randint(1, 2)
+while conf > 0:
+    print('Осталось конфет: ', conf)
+    
+    if player == 1:
+        player = 2
+    else:
+        player = 1
+    num = int(input(f'Ход у {player}-ого игрока: '))
+    while num < 1 or num > step or num > conf:
+        if num > conf:
+            print(f'Осталось только {conf} конфеты!')
+        else:
+            print(f'Вы можете взять не более {step} конфет! Осталось конфет: {conf}')
+        num = int(input(f'Ход у {player}-ого игрока: '))
+    else:
+        conf -= num
+
+print('Победил: ', player)
